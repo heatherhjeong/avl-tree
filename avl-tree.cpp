6771950c13cd01ba83ef345673e361dfc06b1fe2
heather jeong <heatherjeong@heathers-MacBook-Air.local>
2017-04-22 07:32:49 -0400

    implemented all member functions of AVLTree

--- avl-tree.cpp
+++ avl-tree.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <list>
 using namespace std;
 
 template <class T> class Node;
@@ -17,7 +19,7 @@ private:
   int balanceFactor;
 
 public:
-  Node(T d = T(), Node<T>* p =nullptr, Node<T>* l = nullptr, Node<T>* r = nullptr);
+  Node(T d = T(), Node<T>* p = nullptr, Node<T>* l = nullptr, Node<T>* r = nullptr);
 
 };
 
@@ -47,7 +49,7 @@ private:
   void printInOrder(Node<T>* node) const;
   void printPostOrder(Node<T>* node) const;
   void printPreOrder(Node<T>* node) const;
-  void printLevelOrder(Node<T>* node) const;
+  void printLevelOrder(Node<T>* node) const { printLevelOrder(root); }
   Node<T>* copy(Node<T>* rhs) const;
   void remove(Node<T>* node);
 
@@ -63,14 +65,244 @@ public:
   void insert(T data);
   Node<T>* search(const T& data) const;
   void remove(const T& data) { remove(search(data)); }
-  void printInOrder() const { printInOrder(root); }
-  void printPostOrder() const { printPostOrder(root); }
-  void printPreOrder() const { printPreOrder(root); }
-  void printLevelOrder() const { printLevelOrder(root); }
+  void printInOrder() const;
+  void printPostOrder() const;
+  void printPreOrder() const;
+  void printLevelOrder() const;
   int getHeight() const { return getHeight(root); }
   int getSize() const { return size; }
 
 };
+
+template <class T>
+int AVLTree<T>::setBalance(Node<T>* node){
+  if (node == nullptr) return 0;
+  int leftHeight = getHeight(node->left);
+  int rightHeight = getHeight(node->right);
+  node->balanceFactor = rightHeight - leftHeight;
+  return node->balanceFactor;
+}
+
+template <class T>
+int AVLTree<T>::getHeight(Node<T>* node){
+  if (node == nullptr) return -1;
+  return 1 + max(getHeight(node->left), getHeight(node->right));
+}
+
+template <class T>
+void AVLTree<T>::rebalance(Node<T>* node){
+  while (node != nullptr){
+    setBalance(node);
+    if (node->balanceFactor >= 2 || node->balanceFactor <= -2)
+      rotate(node);
+    node = node->parent;
+  }
+}
+
+template <class T>
+void AVLTree<T>::rotate(Node<T>* parent){
+  Node<T>* node;
+  //heavier on the left side
+  if (parent->balanceFactor < -1){
+    node = parent->left;
+    setBalance(node);
+    //left right rotation!
+    if (node->balanceFactor == 1)
+      LRR(parent, node, node->right);
+    //left left rotation
+    else
+      LLR(parent, node, node->left);
+  }
+  //heavier on the right side
+  else {
+    node = parent->right;
+    setBalance(node);
+    //right left rotation
+    if (node->balanceFactor == -1)
+      RLR(parent, node, node->left);
+    //right right rotation!
+    else
+      RRR(parent, node, node->right);
+  }
+}
+
+template <class T>
+void AVLTree<T>::LLR(Node<T>* parent, Node<T>* node, Node<T>* child){
+  Node<T>* grandParent = parent->parent;
+  node->parent = grandParent;
+  Node<T>* nodeRight = node->right;
+  if (nodeRight != nullptr) nodeRight->parent = parent;
+  node->right = parent;
+  parent->parent = node;
+  parent->left = nodeRight;
+  if (grandParent == nullptr){
+    root = node;
+  }
+  else if (grandParent->left == parent){
+    grandParent->left = node;
+  }
+  else {
+    grandParent->right = node;
+  }
+}
+
+template <class T>
+void AVLTree<T>::LRR(Node<T>* parent, Node<T>* node, Node<T>* child){
+  RRR(node, child, child->right);
+  LLR(parent, child, node);
+}
+
+template <class T>
+void AVLTree<T>::RRR(Node<T>* parent, Node<T>* node, Node<T>* child){
+  Node<T>* grandParent = parent->parent;
+  node->parent = grandParent;
+  Node<T>* nodeLeft = node->left;
+  if (nodeLeft != nullptr) nodeLeft->parent = parent;
+  node->left = parent;
+  parent->parent = node;
+  parent->right = nodeLeft;
+  //we are rotating with respect to root, so the root needs to be updated!
+  if (grandParent == nullptr){
+    root = node;
+  }
+  else if (grandParent->left == parent){
+    grandParent->left = node;
+  }
+  else {
+    grandParent->right = node;
+  }
+}
+
+template <class T>
+void AVLTree<T>::RLR(Node<T>* parent, Node<T>* node, Node<T>* child){
+  LLR(node, child, child->left);
+  RRR(parent, child, node);
+}
+
+template <class T>
+void AVLTree<T>::printInOrder(Node<T>* node) const {
+  if (node == nullptr) return;
+  printInOrder(node->left);
+  cout << node->data << ' ';
+  printInOrder(node->right);
+}
+
+template <class T>
+void AVLTree<T>::printPostOrder(Node<T>* node) const {
+  if (node == nullptr) return;
+  printPostOrder(node->left);
+  printPostOrder(node->right);
+  cout << node->data << ' ';
+}
+
+template <class T>
+void AVLTree<T>::printPreOrder(Node<T>* node) const {
+  if (node == nullptr) return;
+  cout << node->data << ' ';
+  printPreOrder(node->left);
+  printPreOrder(node->right);
+}
+
+template <class T>
+void AVLTree<T>::printLevelOrder(Node<T>* node) const {
+  if (node == nullptr) return;
+  list<Node<T>*> nodeQueue;
+  list<int> levelQueue;
+  nodeQueue.push_back(node);
+  levelQueue.push_back(0);
+  while (!nodeQueue.empty()){
+    Node<T>* currNode = nodeQueue.front();
+    int currLevel = levelQueue.front();
+    nodeQueue.pop_front();
+    levelQueue.pop_front();
+    if (currNode->left != nullptr){ 
+      nodeQueue.push_back(currNode->left);
+      levelQueue.push_back(currLevel + 1);
+    }
+    if (currNode->right != nullptr){
+      nodeQueue.push_back(currNode->right);
+      levelQueue.push_back(currLevel + 1);
+    } 
+    for (int i = 0; i < currLevel; i++)
+      cout << '\t';
+    cout << currNode->data << endl;
+  }
+}
+
+template <class T>
+Node<T>* AVLTree<T>::copy(Node<T>* rhs) const {
+  if (rhs == nullptr) return nullptr;
+  Node<T>* ret = new Node<T>(rhs->data, nullptr, copy(rhs->left), copy(rhs->right));
+  if (ret->left != nullptr)
+    ret->left->parent = ret;
+  if (ret->right != nullptr)
+    ret->right->parent = ret;
+  return ret;
+}
+
+template <class T>
+void AVLTree<T>::remove(Node<T>* node){
+  if (node == nullptr) return;
+  if (root == nullptr) return;
+
+  Node<T>* parent = node->parent;
+
+  if (node->left == nullptr && node->right == nullptr){
+    if (parent == nullptr) 
+      root = nullptr;
+    else if (parent->left == node) 
+      parent->left = nullptr;
+    else
+      parent->right = nullptr;
+    delete node;
+    rebalance(parent);
+    size--;
+  }
+
+  else if (node->right == nullptr){
+    if (parent == nullptr){
+      root = node->left;
+      root->parent = nullptr;
+    }
+    else if (parent->left == node){
+      parent->left = node->left;
+      node->left->parent = parent;
+    }
+    else {
+      parent->right = node->left;
+      node->left->parent = parent;
+    }
+    delete node;
+    rebalance(parent);
+    size--;
+  }
+
+  else if (node->left == nullptr){
+    if (parent == nullptr){
+      root = node->right;
+      root->parent = nullptr;
+    }
+    else if (parent->left == node){
+      parent->left = node->right;
+      node->right->parent = parent;
+    }
+    else {
+      parent->right = node->right;
+      node->right->parent = parent;
+    }
+    delete node;
+    rebalance(parent);
+    size--;
+  }
+
+  else {
+    Node<T>* temp = node->right;
+    while (temp->left != nullptr)
+      temp = temp->left;
+    node->data = temp->data;
+    remove(temp);
+  }
+}
 
 template <class T>
 AVLTree<T>::~AVLTree(){
@@ -134,4 +366,22 @@ Node<T>* AVLTree<T>::search(const T& data) const {
     else temp = temp->left;
   }
   return nullptr;
+}
+
+template <class T>
+void AVLTree<T>::printInOrder() const {
+  printInOrder(root);
+  cout << endl;
+}
+
+template <class T>
+void AVLTree<T>::printPostOrder() const {
+  printPostOrder(root);
+  cout << endl;
+}
+
+template <class T>
+void AVLTree<T>::printPreOrder() const {
+  printPreOrder(root);
+  cout << endl;
 }
